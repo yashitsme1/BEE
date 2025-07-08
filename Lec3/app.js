4930ecb024d662da645633aa84f9a13df09d984a
yashitsme1 <yashitsme1@gmail.com>
2025-07-08 10:17:11 +0530

    new

---
+++ Lec3/app.js
@@ -0,0 +1,2 @@
+const sum = require('lec3-yash2264');
+console.log(sum(5, 10));
--- Lec3/index.js
+++ Lec3/index.js
@@ -1,4 +1,5 @@
-function sum(a,b){
-    return a+b;
+function sum(a, b) {
+    return a + b;
 }
-module.export= sum;
+
+module.exports = sum;
