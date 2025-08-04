3ec8284ec93b24b0ee10335f528340659354ba2e
yashitsme1 <yashitsme1@gmail.com>
2025-08-04 15:44:05 +0530

    new

--- Lec13/index.js
+++ Lec13/index.js
@@ -1,34 +1,16 @@
-//fetch mai 2 baar .then lagta hai
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.get('/users', (req, res) => {
+    fs.readFile('./user.json', 'utf8', function(err, data){
+        if(err) res.send(err);
+        let allUsers = JSON.parse(data);
+        res.json(allUsers);
+    });
+});
 
-function getUserData(URL) {
-   fetch(URL)
-    .then((res) => {
-        console.log(res);
-        return res.json()
-    })
-    .then((data) => {
-        console.log(data);
-        data.forEach(user => {
-            displayUser(user);
-        });
-    })
-    .catch((err) => {
-        console.error(err);
-    })
-}
-
-let userConatiner = document.querySelector('.user-container');
-function displayUser(user){
-  let li = document.createElement('li');
-  li.setAttribute('class', 'user-item');
-  li.innerHTML = `<div class="user-info">
-                <h1>${user.name}</h1>
-                <p>${user.username}</p>
-            </div>
-            <div class="user-btn">
-                <button class="user-delete">Delete</button>
-                <button class="user-edit">Edit</button>
-            </div>`
-            userConatiner.appendChild(li);
-}
-getUserData('https://jsonplaceholder.typicode.com/users');
+app.listen(3000,()=>{
+    console.log('Server is running')
+});
